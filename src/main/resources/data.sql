538ccae3201818691395c3c4a17476314e9c4e5f
Ryota Okumura <ryota@RyotanoMacBook-Air.local>
2026-04-15 01:01:46 +0900

    fix SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,33 @@
--- ↓8 rooms, 8 lines below
-INSERT INTO rooms (name, description, price, image_url, capacity) VALUES 
-('Okinawa Room', '...', 250.00, '/src/assets/img/rooms/1.png', 2),
-('Salt Flat Reflection', '...', 120.00, '/src/assets/img/rooms/2.png', 2),
-('Cancun Room', '...', 200.00, '/src/assets/img/rooms/3.png', 4),
-('Sunken Ship Room', '...', 500.00, '/src/assets/img/rooms/4.png', 2),
-('Bali Island Room', '...', 80.00, '/src/assets/img/rooms/5.png', 5),
-('Mangrove Swamp Room', '...', 344.00, '/src/assets/img/rooms/6.png', 6),
-('Deep Sea Room', '...', 389.00, '/src/assets/img/rooms/7.png', 7),
-('Mediterranean Room', '...', 499.00, '/src/assets/img/rooms/8.png', 8);
+-- ↓8 rooms, and pictures are 2 * 8 = 16 rows
+INSERT INTO rooms (name, description, price, image_url, large_image_url, capacity) VALUES 
+('Okinawa Room', '...', 250.00, 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/1.png', 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/1-lg.png', 2),
+
+('Salt Flat Reflection', '...', 120.00, 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/2.png', 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/2-lg.png', 2),
+
+('Cancun Room', '...', 200.00, 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/3.png', 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/3-lg.png', 4),
+
+('Sunken Ship Room', '...', 500.00, 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/4.png', 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/4-lg.png', 2),
+
+('Bali Island Room', '...', 80.00, 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/5.png', 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/5-lg.png', 5),
+
+('Mangrove Swamp Room', '...', 344.00, 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/6.png', 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/6-lg.png', 6),
+
+('Deep Sea Room', '...', 389.00, 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/7.png', 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/7-lg.png', 7),
+
+('Mediterranean Room', '...', 499.00, 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/8.png', 
+ 'https://raw.githubusercontent.com/Flying-WhaleShark/HotelBooking_UI/main/src/assets/img/rooms/8-lg.png', 8);
